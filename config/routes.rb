01e12401a3ca7f042ba2987ee1c01b31f25344c2
Marisol Smith <marisolsmith@gmail.com>
2018-07-27 21:59:38 -0400

    third

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   resources :activities, only: :index do
   resources :checklists, only: :create
   end
-  resources :checklists, only: [:index, :destroy]
+  resources :checklists, only: :index do
+    member do
+      delete 'undo_checklist'
+    end
+  end
+    
+
   root to: 'activities#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
